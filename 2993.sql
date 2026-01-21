09f407a5fddbd7dc267a40d83fa43b1b497643fc
alice <aliceneumann3@gmail.com>
2026-01-21 10:43:13 -0300

    Mais Frequente

---
+++ 2993.sql
@@ -0,0 +1,12 @@
+create table value_table(
+	amount integer
+);
+
+insert into value_table(amount) 
+values(4),(6),(7),(1),(1),(2),(3),(2),(3),(1),(5),(6),(1),(7),(8),(9),(10),(11),(12),(4),(5),(5),(3),(6),(2),(2),(1);
+
+select amount 
+from value_table
+group by amount
+order by count(amount) desc
+limit 1;
